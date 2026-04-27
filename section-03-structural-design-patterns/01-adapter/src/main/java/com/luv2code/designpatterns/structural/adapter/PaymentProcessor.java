953b4adffc12092d126ff7eca898bdc90978e85a
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-04-27 14:36:58 +0100

    Adding section-03-structural-design-patterns-01-adapter

---
+++ section-03-structural-design-patterns/01-adapter/src/main/java/com/luv2code/designpatterns/structural/adapter/PaymentProcessor.java
@@ -0,0 +1,15 @@
+package com.luv2code.designpatterns.structural.adapter;
+
+import java.math.BigDecimal;
+
+/**
+ * Target Interface (PaymentProcessor)
+ *
+ * Defines the standard interface expected by the client (PaymentService)
+ * All adapters will implement this interface to make compatible APIs work
+ */
+public interface PaymentProcessor {
+
+    void pay(BigDecimal amount);
+
+}
---
+++ section-03-structural-design-patterns/01-adapter/src/main/java/com/luv2code/designpatterns/structural/adapter/PaymentService.java
@@ -0,0 +1,17 @@
+package com.luv2code.designpatterns.structural.adapter;
+
+import java.math.BigDecimal;
+
+/**
+ * Client (PaymentService)
+ *
+ * Uses the PaymentProcessor interface to process payments
+ * It is unaware of the underlying payment process or adapter.
+ */
+public class PaymentService {
+
+    public void processPayment(PaymentProcessor paymentProcessor, BigDecimal amount) {
+        paymentProcessor.pay(amount);
+    }
+
+}
