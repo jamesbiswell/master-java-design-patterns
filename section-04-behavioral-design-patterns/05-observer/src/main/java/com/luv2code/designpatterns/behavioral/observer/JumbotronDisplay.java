3419dce940a9d10c11e672ffd82542063b0a6abb
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-04-28 11:22:43 +0100

    Adding section-04-behavioral-design-patterns-05-observer

---
+++ section-04-behavioral-design-patterns/05-observer/src/main/java/com/luv2code/designpatterns/behavioral/observer/JumbotronDisplay.java
@@ -0,0 +1,19 @@
+package com.luv2code.designpatterns.behavioral.observer;
+
+/**
+ * Role: Concrete Observer
+ *
+ * Represents a large stadium scoreboard display.
+ * Reacts to score updates by visually showing changes.
+ */
+public class JumbotronDisplay implements ScoreObserver {
+
+    @Override
+    public void updateScore(int homeTeamScore, int awayTeamScore) {
+
+        System.out.println("[Jumbotron Display] Home: "
+                + homeTeamScore + " | Away: " + awayTeamScore);
+
+    }
+
+}
---
+++ section-04-behavioral-design-patterns/05-observer/src/main/java/com/luv2code/designpatterns/behavioral/observer/ScoreSubject.java
@@ -0,0 +1,15 @@
+package com.luv2code.designpatterns.behavioral.observer;
+
+/**
+ * Role: Subject (Publisher)
+ *
+ * Maintains a list of observers interested in score updates.
+ * Notifies all registered observers when the score changes.
+ */
+public interface ScoreSubject {
+
+    void registerObserver(ScoreObserver observer);
+    void removeObserver(ScoreObserver observer);
+    void notifyObservers();
+
+}
