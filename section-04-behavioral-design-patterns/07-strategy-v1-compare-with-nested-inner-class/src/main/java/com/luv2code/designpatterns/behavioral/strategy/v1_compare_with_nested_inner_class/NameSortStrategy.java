1b17eb3dbfa4d5f034425be5103b6f6c3e626f50
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-04-28 13:09:27 +0100

    Adding section-04-behavioral-design-patterns-07-strategy-v1-compare-with-nested-inner-class

---
+++ section-04-behavioral-design-patterns/07-strategy-v1-compare-with-nested-inner-class/src/main/java/com/luv2code/designpatterns/behavioral/strategy/v1_compare_with_nested_inner_class/NameSortStrategy.java
@@ -0,0 +1,36 @@
+package com.luv2code.designpatterns.behavioral.strategy.v1_compare_with_nested_inner_class;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Role: Concrete Strategy
+ *
+ * Implements sorting algorithm for courses by name.
+ * Sorts courses alphabetically in ascending order.
+ */
+public class NameSortStrategy implements SortStrategy {
+
+    // Define a custom comparator that decides ordering based on the course name
+    // inner class
+    class NameComparator implements Comparator<Course> {
+
+        // returns: negative if course1 < course2, zero if equal, positive if course1 > course2
+        @Override
+        public int compare(Course course1, Course course2) {
+
+            // Uses the String method: compareTo()
+            return course1.getName().compareTo(course2.getName());
+
+        }
+
+    }
+
+    private Comparator<Course> nameComparator = new NameComparator();
+
+    @Override
+    public void sort(List<Course> courses) {
+        courses.sort(nameComparator);
+    }
+
+}
---
+++ section-04-behavioral-design-patterns/07-strategy-v1-compare-with-nested-inner-class/src/main/java/com/luv2code/designpatterns/behavioral/strategy/v1_compare_with_nested_inner_class/SortStrategy.java
@@ -0,0 +1,15 @@
+package com.luv2code.designpatterns.behavioral.strategy.v1_compare_with_nested_inner_class;
+
+import java.util.List;
+
+/**
+ * Role: Strategy
+ *
+ * Defines the interface for different sorting algorithms.
+ * Allows the sorting behavior to be selected at runtime.
+ */
+public interface SortStrategy {
+
+    void sort(List<Course> courses);
+
+}
