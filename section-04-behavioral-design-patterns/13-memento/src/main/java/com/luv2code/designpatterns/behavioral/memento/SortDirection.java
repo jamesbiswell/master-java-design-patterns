bc024c0086dd6698970ac7214d659a308607a1f8
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-04-29 12:56:24 +0100

    Adding section-04-behavioral-design-patterns-13-memento

---
+++ section-04-behavioral-design-patterns/13-memento/src/main/java/com/luv2code/designpatterns/behavioral/memento/SortDirection.java
@@ -0,0 +1,9 @@
+package com.luv2code.designpatterns.behavioral.memento;
+
+/**
+ * Represents the sort direction for displaying courses.
+ */
+public enum SortDirection {
+    ASCENDING,
+    DESCENDING
+}
